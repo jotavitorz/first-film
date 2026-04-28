e135c53253e49f86845d28c63386690c9a08ee20
jotavitorz <joaovitorxz812@gmail.com>
2026-04-27 23:13:43 -0300

    feat: integrate layout with application routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,20 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { Layout } from "../components/layout/index";
 import { Home } from "../pages/Home";
 import { Favorites } from "../pages/Favorites";
 import { MovieDetail } from "../pages/MovieDetail";
 
 export const routes = createBrowserRouter([
     {
-        path: "/",
-        element: <Home />
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />                
+            },
+            {
+                path: "/favorites",
+                element: <Favorites />                
+            },
+            {
+                path: "/movie/:id",
+                element: <MovieDetail />                
+            }
+        ]
     },
-    {
-        path: "/favorites",
-        element: <Favorites />
-    },
-    {
-        path: "/movie/:id",
-        element: <MovieDetail />
-    }
 ])
