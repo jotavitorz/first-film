b255022682ca97eac991106ef25bc589a777e839
jotavitorz <joaovitorxz812@gmail.com>
2026-04-30 15:18:53 -0300

    feat: add header component to application layout

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom"
+import { FaUserAstronaut, FaFilm } from "react-icons/fa";
 
 export function Header(){
+
     return (
-        <header>
-            <h1>First Film</h1>
-        </header>
+        <div className="h-20 w-full px-4 bg-linear-to-b from-indigo-950 to-slate-950 from-60%">
+            <header className="h-20 w-full max-w-7xl flex items-center gap-2 justify-between mx-auto">
+
+                <div className="flex items-center gap-2">
+                    <FaFilm className="text-indigo-500 text-3xl sm:text-[40px]" />
+                    <Link to="/" className="text-neutral-200 text-2xl font-bold sm:text-3xl">
+                        FIRST  
+                        <span className="text-indigo-500"> FILM</span>
+                    </Link>
+                </div>
+
+                <nav>   
+                    <Link to="/favorites" className="text-neutral-200 flex items-center gap-2">
+                        <FaUserAstronaut className="text-2xl md:text-[42px]" />
+                        <span className="font-bold text-indigo-500">User</span>
+                    </Link>
+                </nav>
+
+            </header>
+        </div>
     )
 }
