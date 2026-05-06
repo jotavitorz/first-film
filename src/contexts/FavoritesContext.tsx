b01ed6275d332e8ae868b10f2dcaba076042c483
jotavitorz <joaovitorxz812@gmail.com>
2026-05-06 10:54:52 -0300

    feat: integrate api data fetching with context state

--- src/contexts/FavoritesContext.tsx
+++ src/contexts/FavoritesContext.tsx
@@ -1,7 +1,26 @@
-import { createContext, useState, type ReactNode } from "react";
+import { createContext, useState, useEffect, type ReactNode } from "react";
+import { api } from "../services/tmdbService";
+
+interface FilmesProps {
+    id: number;
+    overview: string;
+    backdrop_path: string;
+    original_title: string;
+    poster_path: string;
+    title: string;
+    vote_average: number;
+    release_date: string;
+    adult: boolean;
+    genre_ids: number[];
+    original_language: string;
+    popularity: number;
+    softcore: boolean;
+    video: boolean;
+    vote_count: number;
+}
 
 interface FavoritesProps {
-    filmes: string[];
+    filmes: FilmesProps[];
 }
 
 const FavoritesContext = createContext({} as FavoritesProps);
@@ -11,8 +30,32 @@ interface ChildrenProviderProps {
 }
 
 export function FavoritesProvider({ children}: ChildrenProviderProps) {
-    const [filmes, setFilmes] = useState([]);
+    const [filmes, setFilmes] = useState<FilmesProps[]>([]);
 
+    useEffect(() => {
+
+        async function loadFilm() {
+            
+            try {
+                const response = await api.get("movie/now_playing", {
+                    params: {
+                        api_key: import.meta.env.VITE_API_KEY,
+                        language: "pt-BR",
+                        page: 1,
+                    }
+                })
+
+                const allFilm = response.data.results.slice(0, 10);
+                setFilmes([...filmes, ...allFilm]);
+
+            }catch(error) {
+                console.log("Error: " + error)
+            }
+        }
+
+        loadFilm();
+
+    }, [])
 
     return (
         <FavoritesContext.Provider value={{filmes}}>
