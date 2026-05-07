2f7a1307794abf4f4c4ae3c3adb4f5007597bcb7
jotavitorz <joaovitorxz812@gmail.com>
2026-05-07 14:53:31 -0300

    feat: add movie navigation and improve home page styling

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,7 @@ export function Home() {
 
     if(filmes.length === 0){
         return (
-            <div className="">
+            <div>
                 <h2 className="pt-10 text-center text-neutral-200 text-3xl font-bold">Carregando Filmess...</h2>
             </div>
         )
@@ -41,15 +41,15 @@ export function Home() {
 
                             <div className="flex gap-8 mt-6 lg:mt-8">
                                 <button className="cursor-pointer bg-indigo-500 py-2 px-8 font-bold rounded-md">Trailer</button>
-                                <Link to="/movie/fordvsferrari" className="cursor-pointer bg-indigo-500 py-2 px-8 font-bold rounded-md">Detalhes</Link>
+                                <Link to={`/movie/${filmeDestaque.id}`} className="cursor-pointer bg-indigo-500 py-2 px-8 font-bold rounded-md">Detalhes</Link>
                             </div>                    
                         </div>
 
-                        <div className="flex-1 w-full h-full text-center">
+                        <div className="flex-1 w-full h-full text-center overflow-hidden">
                             <img 
                                 src={`https://image.tmdb.org/t/p/original${filmeDestaque.backdrop_path}`}
                                 alt="Foto do Filme"
-                                className="rounded-tl-md rounded-tr-md object-cover w-full h-full inline max-w-156.5 max-h-85"
+                                className="rounded-tl-md rounded-tr-md object-cover w-full h-full inline hover:scale-104 duration-300 max-w-156.5 max-h-85 overflow-hidden"
                             />
                             <button 
                                 className="bg-indigo-800 cursor-pointer w-full mb-auto py-2 font-bold rounded-bl-md rounded-br-md transition-all hover max-w-156.5"
@@ -73,21 +73,28 @@ export function Home() {
 
                 <h2 className="text-neutral-200 text-4xl font-bold mb-4">Filmes</h2>
 
-                <section className="grid grid-cols-5 gap-6 text-neutral-200">
+                <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 text-neutral-200">
                 {filmes.map((item) => (
-                    
-                    <div key={item.id} className="mb-10">
-                        <img 
-                            className="object-cover h-full min-h-72 w-full rounded-md"
-                            src={`https://image.tmdb.org/t/p/original${item.poster_path}`} 
-                            alt="Foto do Filme" />
-                        <p className="font-bold text-xl mt-0.5">{item.title}</p>
-                        <div className="flex justify-between px-0.5">
-                            <span>{item.vote_average} / 10</span>
-                            <p className="font-bold text-gray-400">{item.release_date}</p>
-                        </div>
-                    </div>              
-                
+                    <Link to={`/movie/${item.id}`} key={item.id} className="bg-linear-to-b to-indigo-950 from-slate-950 from-40% rounded-md w-full h-full hover:scale-105 transition duration-500">
+
+                        <div className="p-4 flex flex-col gap-2 cursor-pointer flex-wrap">
+
+                            <img 
+                                className="object-cover h-full w-full rounded-md"
+                                src={`https://image.tmdb.org/t/p/original${item.poster_path}`} 
+                                alt={`Filme ${item.title}`}
+                            />
+
+                            <p className="text-lg font-bold">{item.title}</p>
+                            <div className="flex font-bold text-sm gap-1">
+                                <p>⭐ {item.vote_average.toFixed(1)} / 10 •</p>
+                                <span className="text-gray-400">{item.original_language.toLocaleUpperCase()}</span>
+                                <span>•</span>
+                                <p className="text-gray-400">{item.release_date.split("-")[0]}</p>
+                            </div>
+
+                        </div>              
+                    </Link>
                 ))}
                 </section>
                 
