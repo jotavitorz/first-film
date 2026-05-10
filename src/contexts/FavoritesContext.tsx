bf43fe0d685d4d257b8074041a9dd66cb86c0d62
jotavitorz <joaovitorxz812@gmail.com>
2026-05-10 17:20:04 -0300

    feat: implement movie fetch by id in context

--- src/contexts/FavoritesContext.tsx
+++ src/contexts/FavoritesContext.tsx
@@ -10,19 +10,38 @@ interface FilmesProps {
     title: string;
     vote_average: number;
     release_date: string;
+    original_language: string;
+}
+
+interface GenresProps {
+    id: number;
+    name: string;
+}
+
+interface FilmeProps {
+    id: number;
+    overview: string;
+    backdrop_path: string;
+    original_title: string;
+    poster_path: string;
+    title: string;
+    vote_average: number;
+    release_date: string;
     adult: boolean;
     genre_ids: number[];
     original_language: string;
     popularity: number;
     softcore: boolean;
-    video: boolean;
     vote_count: number;
+    genres: GenresProps[];
 }
 
 interface FavoritesProps {
     filmes: FilmesProps[];
     handleLoadMore: () => void;
     loading: boolean;
+    loadFilm: (id: string) => void; 
+    filme: FilmeProps | undefined;
 }
 
 const FavoritesContext = createContext({} as FavoritesProps);
@@ -33,6 +52,7 @@ interface ChildrenProviderProps {
 
 export function FavoritesProvider({ children}: ChildrenProviderProps) {
     const [filmes, setFilmes] = useState<FilmesProps[]>([]);
+    const [filme, setFilme] = useState<FilmeProps | undefined>();
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
 
@@ -59,6 +79,26 @@ export function FavoritesProvider({ children}: ChildrenProviderProps) {
         }
     }
 
+    async function loadFilm(id: string) {
+        setLoading(true);
+
+        try {
+            const response = await api.get(`movie/${id}`, {
+                params: {
+                    api_key: import.meta.env.VITE_API_KEY,
+                    language: "pt-br",
+                }
+            })
+            console.log(response.data);
+            setFilme(response.data);       
+        }catch(error){
+            console.log("Error: " + error);
+            
+        }finally {
+            setLoading(false);
+        }
+    }
+
     useEffect(() => {
         loadFilms(page);
     }, [page])
@@ -70,7 +110,7 @@ export function FavoritesProvider({ children}: ChildrenProviderProps) {
     }
 
     return (
-        <FavoritesContext.Provider value={{filmes, handleLoadMore, loading}}>
+        <FavoritesContext.Provider value={{filmes, handleLoadMore, loading, loadFilm, filme}}>
             {children}
         </FavoritesContext.Provider>
     )
