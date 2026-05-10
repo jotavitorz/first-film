c410d02f5bea5d39966169cabc10606bb18b71c3
jotavitorz <joaovitorxz812@gmail.com>
2026-05-10 17:33:45 -0300

    feat: connect movie details page to context state

--- src/pages/movieDetail/index.tsx
+++ src/pages/movieDetail/index.tsx
@@ -1,8 +1,32 @@
+import { useContext, useEffect} from "react";
+import { FavoritesContext } from "../../contexts/FavoritesContext";
+import { useParams, useNavigate } from "react-router-dom";
 
 export function MovieDetail() {
+    const { loadFilm, filme, loading } = useContext(FavoritesContext);
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    if(!id){
+        return navigate("/", { replace: true});
+    } 
+
+    useEffect(() => {
+        loadFilm(id);
+    }, [id])
+   
+
+    if(loading){
+        return (
+            <div>
+                <h2 className="pt-10 text-center text-neutral-200 text-3xl font-bold">Carregando Filme...</h2>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>Page Movie Detail</h1>
-        </div>
+        <main className="w-full min-h-dvh">
+
+        </main>
     )
 }
