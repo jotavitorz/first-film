bdbf1561cb2cdff759fc5f5cfa08eeb40cf85c1a
jotavitorz <joaovitorxz812@gmail.com>
2026-05-17 17:15:24 -0300

    feat: implement favorite movie actions on favorites page

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { FavoritesContext } from "../../contexts/FavoritesContext";
+import { Link } from "react-router-dom";
 import { FaUserAstronaut } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 
 export function Favorites() {
+    const { favList, deleteFilm, changeWatched } = useContext(FavoritesContext);
+    const watched = favList.filter(item => item.watched === true);
 
     return (
         <main className="w-full min-h-screen">
@@ -11,52 +16,52 @@ export function Favorites() {
                 <div className="flex items-center gap-6 text-neutral-200 font-bold select-none">
 
                     <div className="text-3xl flex flex-col items-center">
-                        <p>10</p>
+                        <p>{favList.length}</p>
                         <span>filmes</span>
                     </div>
 
                     <div className="text-3xl flex flex-col items-center"> 
-                        <p>5</p>
+                        <p>{watched.length}</p>
                         <span>Assistidos</span>
                     </div>
 
                     <div className="text-3xl flex flex-col items-center">
-                        <p>5</p>
+                        <p>{favList.length - watched.length}</p>
                         <span>Assistir</span>
                     </div>
                 </div>
 
             </section>
 
-            <section className="px-4 flex flex-col gap-2 max-w-5xl mx-auto">
+            <section className="px-4 flex flex-col gap-2 max-w-5xl mx-auto text-indigo-200">
 
-                <div className="rounded-md flex my-2 px-6 text-neutral-200 font-bold justify-between shadow-lg shadow-indigo-200">
-                    <div className="flex gap-2 text-xl py-1">
-                        <p>Mario Bros |</p>
-                        <strong> 8.6 / 10</strong>                        
-                    </div>
-                    <div className="flex gap-2 text-xl">
-                        <button className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300">Assistir</button>
-                        <button className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300">Detalhes</button>
-                        <button>
-                            <IoClose size={30} className="cursor-pointer text-red-500" />
-                        </button>
-                    </div>
-                </div>
+                {favList.map((movie) => (
+                    <div 
+                        className={`rounded-md flex my-2 px-6 text-neutral-200 font-bold justify-between shadow-lg ${movie.watched ? "shadow-indigo-600" : "shadow-indigo-200"}`}
+                    >
 
-                <div className="rounded-md flex my-2 px-6 text-neutral-200 font-bold justify-between shadow-lg shadow-sky-200">
-                    <div className="flex gap-2 text-xl py-1">
-                        <p>Mario Bros |</p>
-                        <strong> 8.6 / 10</strong>                        
+                        <div className="flex gap-2 text-xl py-1">
+                            <p>{movie.title} |</p>
+                            <strong> {movie.vote_average.toFixed(1)} / 10</strong>                        
+                        </div>
+                        <div className="flex gap-2 text-xl">
+
+                            <button 
+                                onClick={() => changeWatched(!movie.watched, movie.id)} 
+                                className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300"
+                            >
+                                {movie.watched ? "Assisti" : "Assistir"}
+                            </button>
+
+                            <Link to={`/movie/${movie.id}`} className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300">Detalhes</Link>
+
+                            <button onClick={() => {deleteFilm(movie.id)}}>
+                                <IoClose size={30} className="cursor-pointer text-red-500" />
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex gap-2 text-xl">
-                        <button className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300">Assistir</button>
-                        <button className="cursor-pointer hover:bg-indigo-800 px-2 py-1 rounded-md duration-300">Detalhes</button>
-                        <button>
-                            <IoClose className="cursor-pointer text-red-500" size={30} />
-                        </button>
-                    </div>
-                </div>
+                ))}
+
             </section>
         </main>
     )
