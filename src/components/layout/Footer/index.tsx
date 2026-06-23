e56a7b4124202f47f0e1981fb85660998b641235
jotavitorz <joaovitorxz812@gmail.com>
2026-06-22 22:42:07 -0300

    feat: create application footer

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+
+export function Footer() {
+    return (
+        <footer className="bg-slate-950 h-20 flex flex-col sm:flex-row text-white items-center justify-around px-4 border-t-2 pb-2 sm:pb-2 border-indigo-900 w-full">
+            <h1 className="text-2xl font-bold text-indigo-400">FIRST FILM</h1>
+            <div>
+                <p className="text-gray-400 font-bold">
+                    Todos os direitos reservados <span className="">| @2026</span>
+                </p>
+            </div>
+        </footer>
+    )
+}
